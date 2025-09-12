f7e47e5261a29cc64caad6ae216a8ee4040f7f33
Bjørnar Sundsbø <bjornar.sundsbo@enghouse.com>
2025-09-12 22:14:43 +0200

    not using nullable

--- Source/AutoTemplateSelector.Wpf.Console/TemplateSelector/ConsoleControlTemplateSelector.cs
+++ Source/AutoTemplateSelector.Wpf.Console/TemplateSelector/ConsoleControlTemplateSelector.cs
@@ -1,4 +1,4 @@
-﻿using ActiproSoftware.Windows.Controls.Bars.Mvvm;
+﻿#nullable disable
 using AutoTemplateSelector.Generator;
 using System.Windows;
 using System.Windows.Controls;
@@ -8,28 +8,28 @@ namespace TemplateSelector.Wpf.Console;
 /// <summary>
 /// some doc to be filled in later.
 /// </summary>
-//[AutoTemplateSelector(typeof(string))]
-public partial class ConsoleControlTemplateSelector
+[AutoTemplateSelector(typeof(string))]
+public partial class ConsoleControlTemplateSelector : ItemContainerTemplateSelector
 {
     /// <summary>
     /// Gets or sets template for blue button.
     /// </summary>
-    public ItemContainerTemplate? BlueButtonTemplate { get; set; }
+    public ItemContainerTemplate BlueButtonTemplate { get; set; }
 
     /// <summary>
     /// Gets or sets template for red button.
     /// </summary>
-    public ItemContainerTemplate? RedButtonTemplate { get; set; }
+    public ItemContainerTemplate RedButtonTemplate { get; set; }
 
-    // /// <inheritdoc/>
-    // public override DataTemplate? SelectTemplate(object item, ItemsControl parentItemsControl)
-    // {
-    //     return item switch
-    //     {
-    //         RedBarButtonViewModel => RedButtonTemplate,
-    //         BlueBarButtonViewModel => BlueButtonTemplate,
-    //         _ => base.SelectTemplate(item, parentItemsControl),
-    //     };
-    //
-    // }
+    /// <inheritdoc/>
+    public override DataTemplate SelectTemplate(object item, ItemsControl parentItemsControl)
+    {
+        return item switch
+        {
+            RedBarButtonViewModel => RedButtonTemplate,
+            BlueBarButtonViewModel => BlueButtonTemplate,
+            _ => base.SelectTemplate(item, parentItemsControl),
+        };
+
+    }
 }
